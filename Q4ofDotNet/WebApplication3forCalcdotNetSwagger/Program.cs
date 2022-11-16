ea33bc2a96a966aa7127fff9355e8469f0e239a9
Kanak Kini <Kanak.Kini@emtecinc.com>
2022-11-16 18:58:21 +0530

    assignment 4 of .net

---
+++ Q4ofDotNet/WebApplication3forCalcdotNetSwagger/Program.cs
@@ -0,0 +1,59 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapPost("/calculator", (CalculatorModel calc) =>
+{
+    float ans = ClassLibraryCalcApp.Class1.MathCalc(calc.num1, calc.num2, calc.calculateOpr);
+    return ans;
+
+});
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class CalculatorModel
+{
+    public float num1 { get; set; }
+    public float num2 { get; set; }
+    public string? calculateOpr { get; set; }
+
+
+}
