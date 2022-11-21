478e5367059ed5b7e46ed08380363c607638dbb6
Kanak Kini <Kanak.Kini@emtecinc.com>
2022-11-22 01:12:21 +0530

    adoDotNet assignment by naresh sir

---
+++ AdoDotNet/WebApplication1/Controllers/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApplication1.Model;
+using WebApplication1.Rpositories;
+
+namespace WebApplication1.Controllers
+{
+    
+        [Route("api/[controller]")]
+        [ApiController]
+        public class PlayerController : ControllerBase
+        {
+            private readonly PlayerRepository _playerRepository;
+            public PlayerController(IConfiguration configuration)
+            {
+                _playerRepository = new PlayerRepository(configuration);
+            }
+
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(_playerRepository.Get());
+            }
+
+            [HttpPost]
+            public IActionResult Post(Player player)
+            {
+                return Ok(_playerRepository.Create(player));
+            }
+
+            [HttpPut]
+            public IActionResult Put(Player player, int Id)
+            {
+                return Ok(_playerRepository.Update(player, Id));
+            }
+
+            [HttpDelete]
+            public IActionResult Delete(int Id)
+            {
+                return Ok(_playerRepository.Delete(Id));
+            }
+        }
+    
+}
---
+++ AdoDotNet/WebApplication1/Model/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Model
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PlayerNo { get; set; }
+
+        public DateTime AddedOn { get; set; }
+
+    }
+}
