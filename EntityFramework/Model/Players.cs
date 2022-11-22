7c7ad8186b41ef37b521412ac6eabe6d7a8a6e6e
Kanak Kini <Kanak.Kini@emtecinc.com>
2022-11-22 12:56:36 +0530

    assignment of entityFramework by naresh sir

---
+++ EntityFramework/Model/Players.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EntityFrameworkCore.Model
+{
+    public class Players
+    {
+        public string Name { get; set; }    
+        public string PlayerNo { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
