86ba15e541c573b5e67ebf7f39310f6de0beab4d
Kanak Kini <Kanak.Kini@emtecinc.com>
2022-11-22 13:23:43 +0530

    assignment of entity framework by naresh sir

---
+++ EntityFramework1/Context/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EntityFrameworkCore.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Context
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Players> Players { get; set; }
+        IEnumerable<object> Players { get; }
+
+        Task<int> SaveChanges();
+    }
+}
